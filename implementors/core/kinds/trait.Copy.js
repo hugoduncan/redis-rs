87d96ae39a3bff0984f795a18b1f1df1d8a5cff7
Armin Ronacher <armin.ronacher@active-4.com>
2014-12-24 18:35:24 +0100

    Built documentation

---
+++ implementors/core/kinds/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['redis'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/kinds/trait.Copy.html' title='core::kinds::Copy'>Copy</a> for <a class='enum' href='redis/enum.NumericBehavior.html' title='redis::NumericBehavior'>NumericBehavior</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
